e65dec4b7a9352578c8a04e395aeb18319c44087
Lorenzo Colzani <lollocolz04@gmail.com>
2024-05-16 18:31:09 +0200

    E' VIVO

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
 const { Client, GatewayIntentBits } = require('discord.js');
+const axios = require('axios');
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages]});
+const url_waifu_api = 'https://api.waifu.im/search';
+const client = new Client({ 
+    intents: [
+        GatewayIntentBits.Guilds, 
+        GatewayIntentBits.GuildMessages, 
+        GatewayIntentBits.MessageContent
+    ] 
+});
 
-client.on("messageCreate", (message) => {
-    if (message.content === "!ping") {
-        message.reply("Pong!");
-    }else{
-        message.reply("!ping");
+
+client.on("messageCreate", async (message) => {
+    console.log(message.content);
+    if (message.content === "waifu") {
+        try {
+            // Fai una richiesta POST all'API waifu
+            const response = await axios.get(url_waifu_api);
+            
+            // Ottieni l'URL dell'immagine dalla risposta JSON
+            const imageUrl = response.data.images[0].url;
+            
+            // Invia l'URL dell'immagine come risposta al messaggio
+            message.reply(imageUrl);
+        } catch (error) {
+            // Gestisci gli errori qui
+            console.error('Errore nella richiesta:', error);
+            message.reply("Si è verificato un errore durante la ricerca della waifu.");
+        }
     }
 });
 
-client.login(
-    "MTI0MDU1ODgwOTA2MDA4MTY3NA.Gtsm0m.QgvF8RPUj8oJE3P5RDUwGwIE-UwxbzWsGByG_g"
-)
+client.login("");
